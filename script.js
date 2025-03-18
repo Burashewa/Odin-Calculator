a52fbee900d90c061e0da230d77db6e2d8dbdf92
Biruk Shewafera <125663663+Burashewa@users.noreply.github.com>
2025-03-18 21:57:42 +0300

    Add a function to display inputs into input element.

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
+const input = document.querySelector('#display');
 
+function appendToDisplay(val){
+    if(input.value =='0'){
+        input.value = val
+    }else{
+        input.value +=val
+    }
+   
+}
+
+// function clear(){
+//     input.value = '0';
+// }
 
 
 function Add(num1,num2){
