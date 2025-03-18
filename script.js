cd210800da20a6fd86b7c4534841c45bba22853d
Biruk Shewafera <125663663+Burashewa@users.noreply.github.com>
2025-03-18 18:48:07 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+
+function Add(num1,num2){
+    return num1 + num2;
+}
+
+function Substract(num1,num2){
+    return num1 - num2;
+}
+
+function Multiply(num1,num2){
+    return num1 * num2;
+}
+
+function Divide(num1,num2){
+    return num1 / num2;
+}
