dd0d17bc392f2cd97b1995d9d0bc0b997776ff77
Biruk Shewafera <125663663+Burashewa@users.noreply.github.com>
2025-03-29 15:58:20 +0300

    Refactor addToDisplay function, update styles, and add new HTML element

--- script.js
+++ script.js
@@ -1,21 +1,5 @@
-const input = document.querySelector('#display');
-
-function appendToDisplay(val){
-    if(input.value =='0'){
-        input.value = val
-    }else{
-        input.value +=val
-    }
-   
-}
-
-// function clear(){
-//     input.value = '0';
-// }
-
-
 function Add(num1,num2){
-    return num1 + num2;
+    return parseFloat(num1) + parseFloat(num2);
 }
 
 function Substract(num1,num2){
@@ -29,3 +13,65 @@ function Multiply(num1,num2){
 function Divide(num1,num2){
     return num1 / num2;
 }
+
+let num1 = '';
+let num2 = '';
+let operator = '';
+let result = 0;
+
+function operate(num1,operator,num2){
+
+    if(operator =='+'){
+        result = Add(num1,num2);
+    }
+    else if(operator == '–'){
+        result = Substract(num1,num2);
+    }
+    else if(operator =='×'){
+        result = Multiply(num1,num2);
+    }
+    else if(operator =='÷'){
+        result = Divide(num1,num2);
+    }
+    return input.value = result;
+}
+
+const input = document.querySelector('#display');
+
+function appendToDisplay(val){
+    try{
+        if (!isNaN(val) || val === '.') { 
+
+            if (operator === '') {  
+                num1 += val;    
+                input.value = num1;  
+            } else {  
+                num2 += val ;    
+                input.value = num2;  
+            }  
+        } else if (num1 !== '') {  
+                operator = val;
+                input.value = '';  
+            }
+        
+        else {
+            input.value = 'Error';  
+        }
+    }catch(error){
+        input.value = 'Error';
+    }
+    
+        
+
+}
+
+
+
+function clearBtn(){
+    input.value = '';
+    num1 = '';
+    num2 = '';
+    operator ='';
+}
+
+
